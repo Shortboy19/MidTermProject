c64b34aa6ae625c75d72b229b6883fa9b084f75f
Andrew Levans <AJLevans@student.fullsail.edu>
2020-04-21 18:22:42 -0400

    Moved my files

---
+++ Research/Andrew Research/levelSelector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class levelSelector : MonoBehaviour {
+    public sceneFader fader;
+    public Button[] levelButtons;
+    void Start()
+    {
+        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if(i >= levelReached)
+            levelButtons[i].interactable = false;
+        }
+    }
+
+    public void Select (string levelName)
+    {
+        fader.FadeTo(levelName);
+    }
+}
