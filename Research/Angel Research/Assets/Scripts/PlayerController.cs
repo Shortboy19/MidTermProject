706dd2e25654ea43dab6b7bd1c8980259e4a823b
Angel Rivera Velez <angel.riveravelez3@gmail.com>
2020-04-21 23:21:32 -0400

    Research project that apparently was not committed and pushed - Angel

---
+++ Research/Angel Research/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody player;
+    private Rigidbody rb;
+    public float playerSpeed = 1f;
+    public float turnSpeed;
+    public Vector3 playerPosition;
+    public float timeScale = 3.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(transform.forward * playerSpeed);
+
+        }
+        if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
+        {
+        
+            player.transform.Rotate(playerPosition.x, playerPosition.y - 10 * Time.deltaTime * timeScale - turnSpeed, playerPosition.z);
+        }
+        if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
+        {
+            player.transform.Rotate(playerPosition.x, playerPosition.y + 10 * Time.deltaTime * timeScale + turnSpeed, playerPosition.z);
+        }
+    }
+}
---
+++ Research/Angel Research/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    Transform teleportLoc;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.position = new Vector3(-4,1,15);
+
+        }
+    }
+}
