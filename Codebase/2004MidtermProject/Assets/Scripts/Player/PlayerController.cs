348fb02709cddf90bf7ffeeefb439dbf56c948c1
Griffin DesBles <GADesBles@student.fullsail.edu>
2020-05-07 13:42:03 -0400

    Repushed changes from lastnight

---
+++ Codebase/2004MidtermProject/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//AUTHOR: Griffin DesBles 5/6/2020
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    #region Public Variables
+    [Header("Movement Variables")]
+    [Tooltip("The Component of type CharacterController attached to this object.")]
+    public CharacterController cc;
+    [Tooltip("The mouse sensitivity for looking around. (Defaults to 100)")]
+    [Range(1, 200)]
+    public float LookSenstivity = 100f;
+    [Tooltip("The speed at which the player moves. (Defaults to 5)")]
+    [Range(0,10)]
+    [Space(10)]
+    public float walkSpeed = 5f;
+    [Tooltip("The multiplier to the players speed while sprinting. (Defaults to 2)")]
+    [Range(0, 10)]
+    public float sprintSpeedModifier = 2f;
+    [Tooltip("The ammount of stamina sprinting requires (Defaults to 3)")]
+    public float sprintStaminaCost = 3f;
+    [Tooltip("The height that the player jumps. (Defaults to 3)")]
+    [Range(0, 5)]
+    [Space(10)]
+    public float jumpHeight = 3f;
+    [Tooltip("The ammount of stamina jumping requires (Defaults to 3)")]
+    public float jumpStaminaCost = 3f;
+    [Tooltip("The height that the player jumps. (Defaults to 5)")]
+    [Range(0, 5)]
+    [Space(10)]
+    public float slideDistance = 5f;
+    [Tooltip("The ammount of stamina sliding requires (Defaults to 3)")]
+    public float slideStaminaCost = 3f;
+
+    [Header("Stat Variables")]
+    [Tooltip("The players maximum amount of stamina. (Defaults to 10")]
+    public float maxStamina = 10f;
+    [Tooltip("The players current amount of stamina.")]
+    [HideInInspector] public float currStamina;
+    [Tooltip("The rate at which the players stamina will regen. (Defaults to 1 per second)")]
+    public float staminaRegenRate = 1f;
+
+
+    #endregion
+    #region Private Variables
+    Camera cam;//the players camera
+    float gravity = 9.8f;
+    Vector3 moveDirection = Vector3.zero;
+
+    bool isSprinting = false;
+    bool isSliding = false;
+
+    #endregion
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        cam = GetComponentInChildren<Camera>();
+        currStamina = maxStamina;
+    }
+
+    
+    void Update()
+    {
+        MouseLook();
+        Movement();
+
+
+        if (!isSprinting && !isSliding) { currStamina += staminaRegenRate * Time.deltaTime; }
+        currStamina = Mathf.Clamp(currStamina, 0, maxStamina);
+    }
+
+    void Movement()
+    {
+        if (cc.isGrounded)
+        {
+            //Are we sprinting
+            if (Input.GetButton("Sprint"))
+            {
+                if (currStamina > sprintSpeedModifier)
+                {
+                    isSprinting = true;
+                    currStamina -= sprintStaminaCost * Time.deltaTime;
+                }
+
+            }
+            else { isSprinting = false; }
+
+            moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= walkSpeed * (isSprinting ? sprintSpeedModifier : 1);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                Jump();
+            }
+
+            if (Input.GetButton("Slide"))
+            {
+                if (isSprinting) { Slide(); }
+            } else { cc.height = 2f; isSliding = false; }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        cc.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void MouseLook()
+    {
+        float lookX = Input.GetAxis("Mouse X") * LookSenstivity * Time.deltaTime;
+        float lookY = -Input.GetAxis("Mouse Y") * LookSenstivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * lookX);
+        cam.transform.Rotate(Vector3.right * lookY);
+    }
+
+    void Jump()
+    {
+        if( currStamina > jumpStaminaCost)
+        {
+            moveDirection.y = jumpHeight;
+            currStamina -= jumpStaminaCost;
+        }
+    }
+
+    void Slide()
+    {
+        if(currStamina > slideStaminaCost)
+        {
+            isSliding = true;
+            cc.height = 1f;
+        } else { cc.height = 2f; isSliding = false; }
+    }
+}
