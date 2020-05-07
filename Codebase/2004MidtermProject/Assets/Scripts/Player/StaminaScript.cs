043e9d2b4a3c0fa153c55eb4e4aeeeb526279b46
Andrew Levans <AJLevans@student.fullsail.edu>
2020-05-07 14:32:00 -0400

    Made Stamina Bar.prefab

---
+++ Codebase/2004MidtermProject/Assets/Scripts/Player/StaminaScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaScript : MonoBehaviour
+{
+    public GameObject player;
+    Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.fillAmount = player.GetComponent<PlayerController>().maxStamina/ player.GetComponent<PlayerController>().currStamina;
+    }
+}
