2a6b532c97ea7dfea304023d8945788da879f60a
Christian Chinchilla <COChinchilla@student.fullsail.edu>
2020-05-07 20:08:59 -0400

    The basic Enemy AI works to where it will B line it to the player.

---
+++ Codebase/2004MidtermProject/Assets/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindGameObjectsWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            agent.SetDestination(player.transform.position);
+        }
+    }
+}
