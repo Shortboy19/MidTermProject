a866767b2d06072af96901a6fc2a01022157fe2b
Christian Chinchilla <COChinchilla@student.fullsail.edu>
2020-05-12 13:43:37 -0400

    enemy script got updated

--- Codebase/2004MidtermProject/Assets/Scripts/Enemy.cs
+++ Codebase/2004MidtermProject/Assets/Scripts/Enemy.cs
@@ -7,13 +7,14 @@ public class Enemy : MonoBehaviour
 {
     public NavMeshAgent agent;
     public GameObject player;
+    public Transform spawmpoint; 
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         agent = GetComponent<NavMeshAgent>();
-
+        agent.Warp(spawmpoint.position); 
     }
 
     // Update is called once per frame
