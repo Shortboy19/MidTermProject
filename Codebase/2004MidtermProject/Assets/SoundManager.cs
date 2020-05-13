d91e5d77d9e8314ff67c807801a3a189f0f57d34
Christian Chinchilla <COChinchilla@student.fullsail.edu>
2020-05-13 14:41:51 -0400

    adding the sonic boom sound

---
+++ Codebase/2004MidtermProject/Assets/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    //audio players component.
+    public AudioSource EffectsSounds;
+    public AudioSource MusicSounds;
+
+    //singleton instance
+    public static SoundManager Instance = null;
+
+    //Initialize the singleton instance
+    private void Awake()
+    {
+        //If there is not already an instance of SoundManager, set it to this.
+        if(Instance = null)
+        {
+            Instance = this;
+        }
+        //If an instance already exist, desotry whatever the object is to enforce the singleton.
+        else if(Instance != this)
+        {
+            Destroy(gameObject); 
+        }
+
+        //Set SoundManager to DontDestoryOnLoad so that it wont be destroyed when reloaded.
+        DontDestroyOnLoad(gameObject); 
+    }
+
+    //Play a single Clip through the sound effects source.
+    public void Play(AudioClip clip)
+    {
+        EffectsSounds.clip = clip;
+        EffectsSounds.Play(); 
+    }
+
+    //Play a single Clip through the music source.
+    public void PlayMusic(AudioClip song)
+    {
+        MusicSounds.clip = song;
+        MusicSounds.Play(); 
+    }
+}
