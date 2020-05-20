8ab825ee28665a00fa52c1f03b13e68e8093410c
Christian Chinchilla <COChinchilla@student.fullsail.edu>
2020-05-20 05:30:08 -0400

    now really added the eyes blinking to menu

---
+++ Codebase/2004MidtermProject/Assets/Scripts/Enemy/EnemyBlink.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBlink : MonoBehaviour
+{
+    [SerializeField] Light[] eyes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //eyes = GetComponentsInChildren<Light>();
+        InvokeRepeating("EyesBlinking", 2, Random.Range(1, 6));
+    }
+    void EyesBlinking()
+    {
+        StartCoroutine(Blinking());
+    }
+
+    IEnumerator Blinking()
+    {
+        int num = Random.Range(1, 3);
+        for (int i = 0; i < num; i++)
+        {
+            yield return new WaitForSeconds(0.06f);
+
+            for (int j = 0; j < eyes.Length; j++)
+            {
+                eyes[j].gameObject.SetActive(false);
+            }
+
+            yield return new WaitForSeconds(0.08f);
+
+            for (int j = 0; j < eyes.Length; j++)
+            {
+                eyes[j].gameObject.SetActive(true);
+            }
+        }
+
+    }
+}
