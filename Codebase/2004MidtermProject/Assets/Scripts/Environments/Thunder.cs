490d7d3446f73149a5e7fe45c49b528158ea857f
Christian Chinchilla <COChinchilla@student.fullsail.edu>
2020-05-20 02:09:44 -0400

    the thunder scripts

---
+++ Codebase/2004MidtermProject/Assets/Scripts/Environments/Thunder.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thunder : MonoBehaviour
+{
+    Light[] lights;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lights = GetComponentsInChildren<Light>(); 
+
+        for (int j = 0; j < lights.Length; j++)
+        {
+            lights[j].gameObject.SetActive(false);
+        }
+
+        InvokeRepeating("PlayThunderEffect", 8 , Random.Range(10, 31));
+
+        SoundManager.Instance.PlayEffectAtPoint(SoundManager.Instance.Rain, transform.position); 
+    }
+
+    void PlayThunderEffect()
+    {
+        int i = Random.Range(0, SoundManager.Instance.ThunderClap.Length);
+        SoundManager.Instance.PlayGlobalEffect(SoundManager.Instance.ThunderClap[i]);
+        StartCoroutine(LightFlicker()); 
+    }
+
+    IEnumerator LightFlicker()
+    {
+        float num = Random.Range(0.1f, 0.16f); 
+
+        for (int j = 0; j < lights.Length; j++)
+        {
+            lights[j].gameObject.SetActive(true);
+        }
+
+        yield return new WaitForSeconds(num);
+
+        for (int j = 0; j < lights.Length; j++)
+        {
+            lights[j].gameObject.SetActive(false);
+        }
+
+        yield return new WaitForSeconds(0.05f); 
+
+         num = Random.Range(0.1f, 0.16f);
+
+        for (int j = 0; j < lights.Length; j++)
+        {
+            lights[j].gameObject.SetActive(true);
+        }
+
+        yield return new WaitForSeconds(num);
+
+        for (int j = 0; j < lights.Length; j++)
+        {
+            lights[j].gameObject.SetActive(false);
+        }
+    }
+}
