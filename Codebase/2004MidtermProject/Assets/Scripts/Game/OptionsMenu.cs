c00c12038223fea18a6015fccb5c9b9f17b6a418
Andrew Levans <AJLevans@student.fullsail.edu>
2020-05-26 14:44:29 -0400

    Added a Sensitivity Slider

--- Codebase/2004MidtermProject/Assets/Scripts/Game/OptionsMenu.cs
+++ Codebase/2004MidtermProject/Assets/Scripts/Game/OptionsMenu.cs
@@ -9,16 +9,19 @@ public class OptionsMenu : MonoBehaviour
     [SerializeField] Slider musicVol;
     [SerializeField] Slider effectsVol;
     [SerializeField] Slider ambientVol;
+    [SerializeField] Slider sensitivitySlider;
 
     [SerializeField] TextMeshProUGUI musicVolText;
     [SerializeField] TextMeshProUGUI meffectsVolText;
     [SerializeField] TextMeshProUGUI ambientVolText;
+    [SerializeField] TextMeshProUGUI sensitivityText;
 
     void Start()
     {
         musicVol.value = SoundManager.MusicVolume;
         effectsVol.value = SoundManager.EffectsVolume;
         ambientVol.value = SoundManager.AmbientVolume;
+        sensitivitySlider.value = PlayerController.Player.LookSenstivity/200;
     }
 
     // Update is called once per frame
@@ -27,6 +30,7 @@ public class OptionsMenu : MonoBehaviour
         musicVolText.text = (musicVol.value * 100).ToString("F0");
         meffectsVolText.text = (effectsVol.value * 100).ToString("F0");
         ambientVolText.text = (ambientVol.value * 100).ToString("F0");
+        sensitivityText.text = (sensitivitySlider.value *100).ToString("F0");
     }
 
     public void Apply()
@@ -34,6 +38,7 @@ public class OptionsMenu : MonoBehaviour
         SoundManager.MusicVolume = musicVol.value;
         SoundManager.EffectsVolume = effectsVol.value;
         SoundManager.AmbientVolume = ambientVol.value;
+        PlayerController.Player.LookSenstivity = sensitivitySlider.value * 200;
 
         SoundManager.Instance.UpdateAmbientSoundVolume();
         SoundManager.Instance.UpdateMusicSoundVolume();
