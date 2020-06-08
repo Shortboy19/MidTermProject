ade79f8b09e05c315e2ae3d8e212ae530571bd0f
Christian Chinchilla <COChinchilla@student.fullsail.edu>
2020-06-07 21:49:26 -0400

    Somethings got changed in my scene.

--- Codebase/2004MidtermProject/Assets/LightsOnOff.cs
+++ Codebase/2004MidtermProject/Assets/LightsOnOff.cs
@@ -11,44 +11,47 @@ public class LightsOnOff : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        tutorailLights = GetComponentsInChildren<Light>();
-        for (int i = 0; i < tutorailLights.Length; i++)
-        {
-            tutorailLights[i].gameObject.SetActive(true); 
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("FlashLight"))
         {
-            if (RedLightActive)
-            {
-                for (int i = 0; i < tutorailLights.Length; i++)
-                {
-                    tutorailLights[i].gameObject.SetActive(true);
-                    //PlayerController.Player.frozen = true;
-                    RedLight.gameObject.SetActive(false);
-                    RedLightActive = false; 
-                }
-            }
+            //for(int i = 0; i < tutorailLights.Length; i++)
+            //{
+            //    tutorailLights[i].gameObject.SetActive(false);
+            //    //PlayerController.Player.frozen = true;
+            //}         
+            RedLight.enabled = false;
+            SoundManager.Instance.PlayVoiceLine(7);
+            StartCoroutine(Voiceline()); 
         }
 
         if (other.gameObject.CompareTag("Player"))
         {
-            for (int i = 0; i < tutorailLights.Length; i++)
+            if (RedLightActive)
             {
-                tutorailLights[i].gameObject.SetActive(false);
-                //PlayerController.Player.frozen = true;
-                RedLight.gameObject.SetActive(true);
-                RedLightActive = true;
+                SoundManager.Instance.PlayVoiceLine(8);
             }
         }
+    }
+
+    IEnumerator Voiceline()
+    {
+        PlayerController.Player.frozen = true;
+        while(SoundManager.Instance.VoiceLineSound.isPlaying)
+        {
+            yield return null;
+        }
+
+        RedLightActive = true;
+        PlayerController.Player.frozen = false;
+        DialogBox.ShowWindow("Interaction", "The flashlight can be used on all <color=red>glowing red objects</color> to trigger interactions."); 
     }
 }
