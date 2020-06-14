195a79a048f64979539723b4bc8f6d8154d8dcf9
Griffin DesBles <gadesbles@student.fullsail.edu>
2020-06-14 13:13:10 -0400

    new Build, first WEbGL build

--- Codebase/2004MidtermProject/Assets/Scripts/Environments/Monolith.cs
+++ Codebase/2004MidtermProject/Assets/Scripts/Environments/Monolith.cs
@@ -1,7 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
-using TMPro;
 using UnityEngine;
 
 public class Monolith : MonoBehaviour
--- Codebase/2004MidtermProject/Assets/Scripts/Misc/LifeSaver.cs
+++ Codebase/2004MidtermProject/Assets/Scripts/Misc/LifeSaver.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
+
 using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.Experimental.GlobalIllumination;
 
 public class LifeSaver : MonoBehaviour
 {
