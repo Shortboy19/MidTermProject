fd079ab06888eb23f081d3022cd92a9926e4065b
Griffin DesBles <gadesbles@student.fullsail.edu>
2020-06-16 14:32:23 -0400

    prompt fix

--- Codebase/2004MidtermProject/Assets/Scripts/Tutorials/TrapTutorial.cs
+++ Codebase/2004MidtermProject/Assets/Scripts/Tutorials/TrapTutorial.cs
@@ -18,7 +18,7 @@ public class TrapTutorial : MonoBehaviour
         if (other.gameObject.CompareTag("Player") && !triggered)
         {
             triggered = true;
-            TutorialManager.PlayVoiceLine(9, true, "Traps", "Arm traps by walking up to it and pressing <color=yellow>E</color> when the prompt appears. Traps will arm and automatically trigger when the monster gets close to it.");
+            TutorialManager.PlayVoiceLine(9, true, "Traps", "Arm traps by walking up to it and pressing <color=yellow>E</color> or <color=yellow>RMB</color> when the prompt appears. Traps will arm and automatically trigger when the monster gets close to it.");
             PlayerController.Player.objective.DisplayNewObjective("Arm the trap");
         }
     }
