f27de63c0372216d4db308ff177f95d12719bc39
Griffin DesBles <gadesbles@student.fullsail.edu>
2020-06-18 13:14:56 -0400

    tutorial button only appears after beating tutorial

---
+++ Codebase/2004MidtermProject/Assets/Scripts/Tutorials/MMTutorialChecker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MMTutorialChecker : MonoBehaviour
+{
+    [SerializeField] GameObject button = null;
+    void Start()
+    {
+        if (PlayerPrefs.GetString("TutorialState") == "Complete")
+        {
+            button.SetActive(true);
+        }
+        else
+        {
+            button.SetActive(false);
+        }
+    }
+}
